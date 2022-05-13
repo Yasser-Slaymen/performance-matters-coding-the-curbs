e84ba42b0d4bed1ccc49f905f6182500074998bf
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-05-13 13:42:11 +0200

    minifycss working

--- app.js
+++ app.js
@@ -2,24 +2,41 @@ const express = require('express')
 const app = express()
 const PORT = 2000;
 const bodyParser = require('body-parser')
+const compression = require('compression')
 
 
 // static files
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(compression())
 
 // ejs template engine
 app.set('view engine', 'ejs')
 app.set('views', './views')
+
 // Routes
 const homeRoute = require('./routes/home')
 const addRoute = require('./routes/add')
 const deleteRoute = require('./routes/delete')
 const editRoute = require('./routes/edit')
+
 app.use('/',homeRoute)
 app.use('/',addRoute)
 app.use('/',deleteRoute)
 app.use('/',editRoute)
+
+// cache headers
+
+app.use( function (req, res, next) {
+  // only cache GET requests
+  if(req.method =='GET') {
+    res.set('Cache-control', 'public, max-age=500')
+  } else{
+  // for the other requests set strict nnpm run o caching parameters
+    res.set('Cache-control', `no-store`)
+  }
+  next()
+})
 
 app.listen(PORT, () => {
     console.log(`app listening on port ${PORT}`);
---
+++ minifycss/minify.js
@@ -0,0 +1,18 @@
+const postcss = require('postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const fs = require('fs');
+const cssFile = './public/styles/style.css'
+const minCssFile = 'public/styles/style.mini.css'
+
+// all CSS
+fs.readFile( cssFile, async (err, data) => {
+  const output = await postcss([cssnano, autoprefixer]).process(data);
+  const minifiedCss = output.css;
+
+  fs.writeFile( minCssFile, minifiedCss, err => {
+    if (err) {
+      console.log(err);
+    }
+  })
+});
