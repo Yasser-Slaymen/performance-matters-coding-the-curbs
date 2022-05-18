0cfe4bf5526da62dab137508f4bcb83502747727
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-05-18 23:51:04 +0200

    issues solving

--- minifycss/minify.js
+++ minifycss/minify.js
@@ -3,8 +3,7 @@ const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
 const fs = require('fs');
 const cssFile = './public/styles/style.css'
-// const minCssFile = 'public/styles/style.mini.css'
-const minCssFile = 'public/styles/style.m.css'
+const minCssFile = 'public/styles/style.mini.css'
 
 // all CSS
 fs.readFile( cssFile, async (err, data) => {
