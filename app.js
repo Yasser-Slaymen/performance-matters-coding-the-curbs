b5ed26e897baba4ee5df29bc7a4258bed84697dd
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-06-16 14:51:07 +0200

    search for solution

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 2000;
+const PORT = 3000;
 const bodyParser = require('body-parser')
 const compression = require('compression')
 
